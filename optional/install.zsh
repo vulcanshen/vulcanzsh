598126f54553d74e9c93cfbb58a3e5018122b250
vulcanshen <vulcanshen@infinitix.co>
2025-12-28 21:58:47 +0800

    fix optional zsh install

--- optional/install.zsh
+++ optional/install.zsh
@@ -2,10 +2,7 @@ GITHUB_USER="vulcanshen"
 REPO_NAME="vulcanzsh"
 BRANCH="main"
 BASE_URL="https://raw.githubusercontent.com/${GITHUB_USER}/${REPO_NAME}/${BRANCH}"
-print_success() { echo -e "\e[32m[+] $1\e[0m" }
-print_info()    { echo -e "\e[34m[i] $1\e[0m" }
-print_warn()    { echo -e "\e[33m[!] $1\e[0m" }
-print_error()   { echo -e "\e[31m[-] $1\e[0m" }
+
 MARKER_START="# --- vulcanzsh optional config Start ---"
 MARKER_END="# --- vulcanzsh optional config End ---"
 
@@ -21,7 +18,7 @@ if ! grep -q "$MARKER_START" ~/.zshrc; then
     echo -e "\n$MARKER_START" >> ~/.zshrc
     echo "$LOAD_CMD" >> ~/.zshrc
     echo "$MARKER_END" >> ~/.zshrc
-    print_success "Injection added to ~/.zshrc"
+    echo "Injection added to ~/.zshrc"
 else
-    print_info "Configuration markers found in ~/.zshrc, files updated."
+    echo "Configuration markers found in ~/.zshrc, files updated."
 fi
